5a15755604e507533d09c64675ce4def0599e39d
marcos <marcos@getrak.com.br>
2019-01-17 13:43:33 -0200

    finalizado

--- config.go
+++ config.go
@@ -3,7 +3,7 @@ package main
 
 const serverPort = ":8080"
 
-const redisUrlServer = "192.168.0.51:6379"
+const redisUrlServer = "redis:6379"
 
 const noventena = 7776000 // valor em segundos. 7776000 igual a 90 dias
 
